d05274aa947b027810519721b6415ec38a247427
catenary91 <sweetonion135@gmail.com>
2023-12-30 23:36:56 +0900

    created model

--- menus/models.py
+++ menus/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+MENU_TYPES = {
+    ('B', 'breakfast'),
+    ('L', 'lunch'),
+    ('D', 'dinner'),
+}
+
+class Menu(models.Model):
+    date = models.DateField()
+    mtype = models.CharField(max_length=1, choices=MENU_TYPES)
+    content = models.CharField(max_length=300)
+    calories = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.date} / {MENU_TYPES[self.mtype]}'
