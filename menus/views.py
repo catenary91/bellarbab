5248898e7700060f0ca7613180ac1f1905571830
catenary91 <sweetonion135@gmail.com>
2024-01-01 20:31:14 +0900

    edited today format

--- menus/views.py
+++ menus/views.py
@@ -9,20 +9,30 @@ from .models import Menu
 @csrf_exempt
 def today(request):
     data = json.loads(request.body)
-    open('a.json', 'w').write(json.dumps(data, indent=4))
-    mtype = {'조식': 'B', '석식': 'D'}[data['action']['clientExtra']['mtype']]
+    mtype_o = data['action']['clientExtra']['mtype']
+    mtype = {'조식': 'B', '석식': 'D'}[mtype_o]
     result = Menu.objects.filter(date=datetime.date.today(), mtype=mtype)
-    if result.count() == 1:
-        menu = result[0]
-        return JsonResponse({'answer': f'오늘의 메뉴 : {menu.content}'}) 
-    else:
+    if result.count() != 1:
         return JsonResponse({'answer': '오늘의 메뉴를 찾을 수 없습니다.'})
-
+    
+    else:
+        if mtype=='B':
+            menu = result[0].content.replace(';', '\n      ')
+            i = menu.find(':')
+            menu1 = menu[:i]
+            menu2 = menu[i+1:]
+            return JsonResponse({'answer': f'오늘의 {mtype_o}입니다!\n\n한식\n      {menu1}\n양식\n      {menu2}'}) 
+        else: 
+            menu = result[0].content.replace(';', '\n')
+            return JsonResponse({'answer': f'오늘의 {mtype_o}입니다!\n\n{menu}'})
 
 @csrf_exempt
 def get_menu(request):
-    data = json.loads(request.body)
-    print(data)
+    data = json.loads(request.body)    
+    date = data['action']['parama']['date'].replace(' ', '')
+    mtype = data['action']['parama']['menu_type']
+    
+    
     
     return JsonResponse({'answer': '오늘의 조식입니다!'})
 
