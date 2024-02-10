eebd569fa59f0172410897d05a03d5c8a6edbc99
catenary91 <sweetonion135@gmail.com>
2024-02-10 07:21:31 +0000

    mapped DEBUG to env

--- bellarbab/settings.py
+++ bellarbab/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2k4ink!_4m^((*gcj%f+#f%z!_q)g79g$&=vjfoprqra@^uuf)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') != 'FALSE'
 
 ALLOWED_HOSTS = ['*']
 
