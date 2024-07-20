9c8293e3e109f114fe2ee7a939cd88b7b19da8c8
catenary91 <sweetonion135@gmail.com>
2024-07-21 00:47:19 +0900

    Add CSRF_TRUSTED_ORIGINS

--- bellarbab/settings.py
+++ bellarbab/settings.py
@@ -124,3 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ['http://jinnysoo.iptime.org:8000']
